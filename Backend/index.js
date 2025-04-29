28a84b4bb0c5363a6f092ac683bfb2279afa919e
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2025-04-29 13:13:43 +0530

    solve problme

--- Backend/index.js
+++ Backend/index.js
@@ -24,7 +24,7 @@ app.use("/api/auth", authRouter);
 
 app.use(express.static(path.join(__dirname, '/Frontend/dist')));
 
-app.get('*', (req, res) => {
+app.get(/^\/(?!api|static).*$/, (req, res) => {
     res.sendFile(path.join(__dirname, 'Frontend', 'dist', 'index.html'));
 });
 
