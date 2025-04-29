a696915a0bc63ee23ad7f8433ef97cbe3783765b
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2025-04-29 12:15:17 +0530

    deployment update

---
+++ Backend/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import path from "path";
+import authRouter from "./routes/auth.route.js";
+
+dotenv.config();
+
+const __dirname = path.resolve();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.listen(3000, () => {
+    console.log("server is running at port 3000");
+})
+
+// api routes here
+
+app.use("/api/auth", authRouter);
+
+app.use(express.static(path.join(__dirname, '/Frontend/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Frontend', 'dist', 'index.html'));
+});
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success: false,    
+        statusCode,
+        message,
+    });
+});
